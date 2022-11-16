55a199eb16a876b4bf3714600bc616c158719248
unknown <pramodjadhavnbn@gmail.com>
2022-11-16 19:50:15 +0530

    waitTillElement added in util class

--- src/main/java/Utilclasses/Util1.java
+++ src/main/java/Utilclasses/Util1.java
@@ -66,4 +66,14 @@ public class Util1 {
     	 
     	 return path;
      }
+     
+     public static void waitTillElement(WebDriver driver , WebElement element) 
+ 	
+ 	
+ 	{
+ 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+ 		wait.until(ExpectedConditions.visibilityOf(element));
+ 	}
+     
+     
 }
