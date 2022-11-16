baf7837476d2c75ba4787824d24cb46827856618
unknown <pramodjadhavnbn@gmail.com>
2022-11-16 19:59:07 +0530

    waitTillElement is removed

--- src/main/java/Utilclasses/Util1.java
+++ src/main/java/Utilclasses/Util1.java
@@ -67,13 +67,7 @@ public class Util1 {
     	 return path;
      }
      
-     public static void waitTillElement(WebDriver driver , WebElement element) 
- 	
- 	
- 	{
- 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
- 		wait.until(ExpectedConditions.visibilityOf(element));
- 	}
+     
      
      
 }
